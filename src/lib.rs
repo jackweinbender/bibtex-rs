abe3d38dfa4133ad167308a9d4ae11a73ecdcf3c
Jack Weinbender <jack.weinbender@gmail.com>
2017-03-11 20:13:56 -0600

    Parser files setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod field;
 pub mod entry;
+pub mod parser;
 
 #[cfg(test)] 
 mod test; 
--- src/test.rs
+++ src/test.rs
@@ -1,5 +1,22 @@
 use entry::Entry;
 use field::Field;
+use parser::Parser;
+
+#[test]
+fn parser() {
+    let bib = include_str!("../test/single_sorted.bib");
+    // let mut v = Vec::new();
+    // v.push(Field::new("title", "The Historiographical {Jesus}: Memory, Typology, and the Son of {David}"));
+    // v.push(Field::new("shorttitle", "The Historiographical Jesus"));
+    // v.push(Field::new("author", "Le Donne, Anthony"));
+    // v.push(Field::new("location", "Waco, TX"));
+    // v.push(Field::new("publisher", "Baylor University Press"));
+    // v.push(Field::new("year", "2009"));
+
+    // let mut entry = Entry::new("book", "le-donne2009", v);
+    
+    // assert_eq!(entry, Parser::entry_from_str(bib));
+}
 
 #[test]
 fn serialization() {
@@ -12,7 +29,8 @@ fn serialization() {
     v.push(Field::new("publisher", "Baylor University Press"));
     v.push(Field::new("year", "2009"));
 
-    let mut e = Entry::new("book", "le-donne2009", v);
+    let mut entry = Entry::new("book", "le-donne2009", v);
     
-    assert_eq!(e.to_string(), bib);
+    assert_eq!(entry.to_string(), bib);
 }
+
