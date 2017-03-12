e314129daa1c778f36f4c196e823a58e9a9b5213
Jack Weinbender <jack.weinbender@gmail.com>
2017-03-11 20:15:29 -0600

    added parser.rs

---
+++ src/parser.rs
@@ -0,0 +1,12 @@
+use entry::Entry;
+
+pub struct Parser;
+
+impl Parser {
+    pub fn bib_from_str(input: &str) -> Vec<Entry> {
+        unimplemented!();
+    }
+    pub fn entry_from_str(entry_string: &str) -> Entry {
+        unimplemented!();
+    }
+}
