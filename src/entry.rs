550a13840f81e6e40319fedc1e2c3858a663c513
Jack Weinbender <jack.weinbender@gmail.com>
2017-06-03 16:35:09 -0500

    Rewrite

---
+++ src/entry.rs
@@ -0,0 +1,6 @@
+enum Item {
+    Entry,
+    Comment,
+    Prelude,
+    String,
+}
--- src/parser.rs
+++ src/parser.rs
@@ -2,6 +2,7 @@ pub struct Parser;
 
 impl Parser {
     pub fn parse(input: &str) -> Vec<&str> {
+        
         unimplemented!();
     }
 }
