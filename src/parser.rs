6c00dcf7b217fbf7429522ca137ef5847a314a74
Jack Weinbender <jack.weinbender@gmail.com>
2017-06-03 16:55:08 -0500

    Merge

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-// use entry::Entry;
-
-pub struct Parser;
-
-impl Parser {
-    // pub fn bib_from_str(input: &str) -> Vec<Entry> {
-    //     unimplemented!();
-    // }
-    // pub fn entry_from_str(entry_string: &str) -> Entry {
-    //     unimplemented!();
-    // }
-=======
 pub struct Parser;
 
 impl Parser {
@@ -18,5 +5,4 @@ impl Parser {
         
         unimplemented!();
     }
->>>>>>> 550a13840f81e6e40319fedc1e2c3858a663c513
 }
--- src/test.rs
+++ src/test.rs
@@ -1,30 +1,5 @@
-<<<<<<< HEAD
-use entry::Entry;
-use field::Field;
-// use parser::Parser;
-
-#[test]
-fn parser() {
-}
-
-#[test]
-fn serialization() {
-    let bib = include_str!("../test/single_sorted.bib");
-    let mut fields = Vec::new();
-    fields.push(Field::new("title", "The Historiographical {Jesus}: Memory, Typology, and the Son of {David}"));
-    fields.push(Field::new("shorttitle", "The Historiographical Jesus"));
-    fields.push(Field::new("author", "Le Donne, Anthony"));
-    fields.push(Field::new("location", "Waco, TX"));
-    fields.push(Field::new("publisher", "Baylor University Press"));
-    fields.push(Field::new("year", "2009"));
-
-    let mut entry = Entry::new("book", "le-donne2009", fields);
-    
-    assert_eq!(entry.to_string(), bib);
-=======
 #[test]
 fn parse() {    
     assert_eq!(true, true);
->>>>>>> 550a13840f81e6e40319fedc1e2c3858a663c513
 }
 
