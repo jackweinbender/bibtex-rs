645be88275aeae0518c759cebc8b6512f2301d9b
Jack Weinbender <jack.weinbender@gmail.com>
2017-08-25 21:16:16 -0500

    Scratch

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,57 @@
 use std::str::Chars;
 
 fn main() {
-
-    let mut lib = Library;
-
     let input = include_str!("bib.bib");
     let mut iter = input.chars();
 
-    let mut depth = 0;
+    print!("{:?}\n", parse(iter));
+
+}
+
+fn parse(mut iter: Chars) -> Vec<Entry> {
+    
+    let mut lib = vec![];
 
     while let Some(ch) = iter.next() {
-
         match ch {
-            '@' if depth < 1 => {
-                let (entry, i) = entry(iter.clone());
-                print!("{:?}", entry);
-            },
-            _ => continue,
+            '@' => { 
+                let (entry, i) = next_entry(iter);
+                lib.push(entry);
+                iter = i;
+             },
+             _  => { continue },
         }
 
     }
 
+    lib
 }
+
+fn next_entry(mut iter: Chars) -> (Entry, Chars) {
+    
+    let mut buffer = String::new();
+    let mut brace_depth = 0;
+
+    while let Some(ch) = iter.next() {
+        match ch {
+            '}' if brace_depth <= 1 => { break },
+            '}' => {
+                buffer.push(ch);
+                brace_depth = brace_depth - 1;
+                },
+            '{' => {
+                buffer.push(ch);
+                brace_depth = brace_depth + 1 
+                },
+            _ => buffer.push(ch)
+        }
+    }
+    (Entry{content: buffer}, iter)
+}
+
 #[derive(Debug)]
-struct Library;
-#[derive(Debug)]
-struct Entry;
+struct Entry { content: String }
 
-fn entry(mut iter: Chars) -> (Entry, Chars) {
-
-
-    (Entry, iter)
+impl FromStr for Entry {
+    
 }
