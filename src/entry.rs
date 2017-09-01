72c3dd984f0750782c2242c3de5fb9fd640055ec
Jack Weinbender <jack.weinbender@gmail.com>
2017-08-31 23:02:48 -0500

    Basic (Brittle) EntryItem Parsing

--- src/entry.rs
+++ src/entry.rs
@@ -6,7 +6,60 @@ pub struct EntryItem {
     pub_type: String,
     tags: Vec<Tag>,
 }
-impl EntryItem {}
+impl EntryItem {
+    pub fn new(s: &str) -> EntryItem {
+        let mut t = String::new();
+        let mut k = String::new();
+        let mut tags = vec![];
+        let mut iter = s.char_indices();
+
+        while let Some((idx, ch)) = iter.next() {
+            match ch {
+                '@' if idx == 0 => continue,
+                '{' => break,
+                _ => t.push(ch),
+            }
+        }
+        while let Some((_, ch)) = iter.next() {
+            match ch {
+                '}' => break,
+                ',' => {
+                    if let Some((i, _)) = iter.next() {
+                        let ts = s[i..].trim().trim_right_matches(',');
+
+                        tags = ts[..ts.len() - 1]
+                            .split("\n")
+                            .filter(|x| x.trim() != "")
+                            .map(|x| Tag::new(x.trim_right_matches(',')))
+                            .collect();
+                    };
+                    break;
+                }
+                _ => k.push(ch),
+            }
+        }
+        EntryItem {
+            pub_type: t,
+            key: k,
+            tags: tags,
+        }
+    }
+}
+
+#[test]
+fn parse_entry_item_from_str() {
+    let input = include_str!("../test/single_unsorted.bib");
+    let expected = EntryItem {
+        key: String::from("le-donne2009"),
+        pub_type: String::from("book"),
+        tags: vec![
+            Tag::new("title = {The Historiographical {Jesus}}"),
+            Tag::new("author = {Le Donne, Anthony}"),
+        ],
+    };
+    assert_eq!(EntryItem::new(input), expected);
+}
+
 
 #[derive(Debug, PartialEq)]
 pub struct StringItem {
